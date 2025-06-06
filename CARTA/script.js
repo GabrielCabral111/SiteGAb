af9225ef341fa4bcbea9c14319b17ce1b502cac9
Gabriel Cabral De Paula <gabrielcabral072@gmail.com>
2025-06-06 11:52:21 -0300

    suba

---
+++ CARTA/script.js
@@ -0,0 +1,6 @@
+// Exibe a carta com delay opcional
+window.addEventListener('DOMContentLoaded', () => {
+    const carta = document.getElementById('carta');
+    carta.style.animationDelay = '0.5s';
+  });
+  
